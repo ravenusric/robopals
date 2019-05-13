e14b2443a94d5dffc783eb0997e226c233b00130
ravenusric <richie.roney@gmail.com>
2019-05-13 12:06:54 -0400

    Updates

---
+++ precache-manifest.12489db658656d648636d1e77a8a76b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ddd77aec19cb3d3854e38b8d9cbb470",
+    "url": "/robopals/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robopals/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "3dc118f77364d0c275d1",
+    "url": "/robopals/static/css/main.35215672.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robopals/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "3dc118f77364d0c275d1",
+    "url": "/robopals/static/js/main.a1284df8.chunk.js"
+  },
+  {
+    "revision": "85ca4ef05e834621cc56",
+    "url": "/robopals/static/js/runtime~main.1d0bf8ea.js"
+  },
+  {
+    "revision": "86c545ccae4a76a95a9ffdd312221e69",
+    "url": "/robopals/static/media/MR ROBOT.86c545cc.woff"
+  }
+]);
